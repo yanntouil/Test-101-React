2db81e0ad105d668d6991ca0b9e9c03879ff6027
Yann Touil <yanntouil@gmail.com>
2022-03-30 11:58:03 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./styles.css";
+
+const url = "https://restcountries.com/v3.1/region/europe";
+
+/**
+ * Europe Timezone Conversion ✈️
+ *
+ * - fork this sandbox
+ * - fetch the countries from the provided api endpoint
+ * - show the countries in a table with following props: common name, flag emoji, capital, population, current (converted) local time.
+ * - have a time input that defaults to now. it should update the country times on change, converting the new time to respective local times in the table.
+ *
+ * As a plus:
+ * - prevent the possibility of race conditions in your fetch useEffect
+ * - install any deps you need, axios or whatever
+ *
+ * gl
+ */
+
+const App = () => {
+  return <div></div>;
+};
+
+export default App;
